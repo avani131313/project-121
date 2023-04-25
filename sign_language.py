a3a8313c535f382f91cd51c1530cbc0e8d68ca04
avani131313 <97681549+avani131313@users.noreply.github.com>
2023-04-25 18:02:08 +0400

    Update sign_language.py

--- sign_language.py
+++ sign_language.py
@@ -28,11 +28,11 @@ while True:
             for tip in finger_tips:
                 x,y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
                 cv2.circle(img,(x,y),15,(255,0,0),cv2.FILLED )
-            if lm_list[tip].x < lm_list[tip-3].x:
-                cv2.circle(img, (x,y),15, (0,255,0), cv2.FILLED)
-                finger_fold_status.append(True)
-            else:
-                finger_fold_status.append(False)
+                if lm_list[tip].x < lm_list[tip-3].x:
+                    cv2.circle(img, (x,y),15, (0,255,0), cv2.FILLED)
+                    finger_fold_status.append(True)
+                else:
+                    finger_fold_status.append(False)
                 
             if all(finger_fold_status) :
                 if(lm_list[thumb_tip-1].y < lm_list[thumb_tip-2].y):
@@ -47,8 +47,13 @@ while True:
 
             mp_draw.draw_landmarks(img, hand_landmark,
             mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec((0,0,255),2,2),
+    
             mp_draw.DrawingSpec((0,255,0),4,2))
+         
     
 
     cv2.imshow("hand tracking", img)
-    cv2.waitKey(1)
+    if  cv2.waitKey(1) == 32
+        break
+        
+   
